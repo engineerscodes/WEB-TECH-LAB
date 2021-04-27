133787d2be386befdaefb3a75b5716c88b7eb40a
engineerscodes <practicalengineers05@gmail.com>
2021-04-27 15:56:27 +0530

    git api

--- GITHUB_API/fecthapi.js
+++ GITHUB_API/fecthapi.js
@@ -1,6 +1,3 @@
-
-
-
 /*fetch("https://api.github.com/rate_limit")
   .then((res) => {
     if (res.ok) {
@@ -12,12 +9,30 @@
   })
   .then();*/
 
-  async function CLICK()
-  {
-      let url = "https://api.github.com/engineerscodes/";
-      let respone= await fetch(url);
-      let data= await respone.json()
-      console.log(data)
+var myHeaders = new Headers();
+myHeaders.append("Cookie", "_octo=GH1.1.2112506239.1613242999; logged_in=no");
 
-  }
-CLICK();
+var requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+fetch(
+  "https://api.github.com/repos/engineerscodes/JavaWorkSpaceHUB/stats/contributors",
+  requestOptions
+)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+
+    //let contributorsnames = result[1].author;
+    // console.log(contributorsnames)
+    for (i in result) {
+      
+      console.log(
+        "NAME :" + result[i].author.login + "---> COMMITS :" + result[i].total
+      );
+    }
+  })
+  .catch((error) => console.log("error", error));
---
+++ ML5/sketch.js
@@ -0,0 +1,13 @@
+function preload() {
+ 
+}
+
+function setup() {
+    createCanvas(500,500);
+
+}
+
+function draw() {
+  background(300);
+ 
+}
